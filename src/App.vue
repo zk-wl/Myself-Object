570a0fbb62765af6ca5b3b1932e6300139692be7
zk-wl <wl2521440180@163.com>
2022-07-30 22:40:13 +0800

    第一次提交尚品汇项目代码

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+}
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
